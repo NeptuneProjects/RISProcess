d21779aff1cfe7a5531d6fdf335ae9ebd50d3cd0
NeptuneProjects <william.f.jenkins@gmail.com>
2021-01-22 20:37:46 -0800

    Test

--- RISProcess/commands.py
+++ RISProcess/commands.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
+import numpy as np
+
 def test1():
     print("Hello world, this is Script I.")
+    print(np.arange(1,10))
 
 def test2():
     print("Hello world, this is Script II.")
