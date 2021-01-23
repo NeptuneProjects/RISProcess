d834db56ffc956cb077fbd84ebfedbf54d5f5e8a
NeptuneProjects <william.f.jenkins@gmail.com>
2021-01-22 17:26:04 -0800

    Fix pip package dir

--- setup.py
+++ setup.py
@@ -1,22 +1,28 @@
 from setuptools import setup
 
-setup(
-    name="RISProcess",
-    url="https://github.com/NeptuneProjects/RISProcess.git",
-    author="William F. Jenkins II",
-    author_email="wjenkins@ucsd.edu",
-    packages=["RISCluster"],
-    install_requires=[
-        "h5py",
-        "jupyterlab",
-        "numpy",
-        "obspy",
-        "pandas",
-        "scipy",
-        "tqdm"
-    ],
-    version="0.0b",
-    license="MIT",
-    description="Package provides signal processing workflow for seismic data \
-        collected on the Ross Ice Shelf, Antarctica."
-)
+def setup_package():
+    setup(
+        name="RISProcess",
+        url="https://github.com/NeptuneProjects/RISProcess.git",
+        author="William F. Jenkins II",
+        author_email="wjenkins@ucsd.edu",
+        packages=["RISCluster"],
+        package_dir = {"": "RISCluster"},
+        install_requires=[
+            "h5py",
+            "jupyterlab",
+            "numpy",
+            "obspy",
+            "pandas",
+            "scipy",
+            "tqdm"
+        ],
+        version="0.0b",
+        license="MIT",
+        description="Package provides signal processing workflow for seismic data \
+            collected on the Ross Ice Shelf, Antarctica."
+    )
+
+
+if __name__ == "__main__":
+    setup_package()
