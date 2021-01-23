bb581bd339e95c3d617702f5980ea782388ebcee
NeptuneProjects <william.f.jenkins@gmail.com>
2021-01-22 20:43:32 -0800

    Test argparse

--- RISProcess/commands.py
+++ RISProcess/commands.py
@@ -6,10 +6,10 @@ import numpy as np
 
 def test1():
     parser = argparse.ArgumentParser(description="Test argparse")
-    parser.add_argument("number", type=float, default=8.0)
+    parser.add_argument("--n", type=float, default=8.0)
     args = parser.parse_args()
     print("Hello world, this is Script I.")
-    print(np.square(args.number))
+    print(np.square(args.n))
 
 def test2():
     print("Hello world, this is Script II.")
